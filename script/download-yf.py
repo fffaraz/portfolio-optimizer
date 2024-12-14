eeb9081853d73c0472e8057c951586e73c14e860
Faraz Fallahi <fffaraz@gmail.com>
2024-12-13 21:20:40 -0800

    Update download-yf.py

--- script/download-yf.py
+++ script/download-yf.py
@@ -7,12 +7,14 @@
 # license that can be found in the LICENSE file
 #
 
+# pip3 install --user yfinance
+
 import json
 import time
 import yfinance as yf
 
 
-symbols = ["ABR","AGG","AGNC","AI","AMBA","AMD","AMT","ARKF","ARKG","ARKK","ARKQ","ARKW","BA","BABA","BB","BBIO","BITO","BIV","BLV","BLZE","BND","BNDX","BSV","CCI","CIBR","CNDT","CRM","CRWD","CXSE","DAL","DBX","DEM","DGRO","DHR","DJD","DLR","DOCN","DOCU","DTD","EDV","EMD","EQR","FB","FRT","FSLY","GE","GFS","GOGO","GTLB","HA","HDV","HOOD","IAGG","ICE","IECS","IEUR","IGIB","IGLB","IGSB","ILMN","ILTB","IMTB","INTC","IPAC","IPO","ISTB","ITOT","IUSB","IUSG","IUSV","IVV","IVW","IWB","IWV","IXUS","JHMM","JPSE","LAZR","LUV","MCHP","MGK","MGV","MNDT","MNDY","MSFT","MU","MUB","NET","NFLX","NIO","NNN","NSTB","NVTA","O","ORCL","PANW","PD","PFE","PFF","PLTR","PYPL","QQQ","QQQE","QYLD","REET","RSP","RWR","SCHB","SCHD","SCHG","SCHH","SCHK","SCHX","SCHY","SGOL","SIVR","SLQD","SOFI","SPHD","SPHY","SPLG","SPOT","SPTL","SPTM","SPY","SPYD","SQ","STAG","STIP","STOR","STX","T","TIP","TSM","TWTR","USHY","USRT","VAW","VB","VBK","VBR","VCIT","VCLT","VCR","VCSH","VDC","VDE","VEA","VEU","VFH","VGT","VHT","VIG","VIS","VLDR","VMBS","VNQ","VNQI","VO","VOE","VONE","VOO","VOT","VOX","VPU","VRM","VSS","VT","VTEB","VTI","VTV","VV","VWO","VXF","VXUS","VYM","VYMI","WDC","WMT","WPC","XLC","XLE","XLNX","Z","ZEN","ZG","ZM"]
+symbols = ["AGG", "ARKF", "ARKG", "ARKK", "BIV", "BLV", "BLZE", "BND", "BNDX", "BSV", "CIBR", "CRWD", "CXSE", "DEM", "DGRO", "DIVB", "DIVO", "DJD", "DOCN", "DTD", "EDV", "HDV", "IAGG", "IEUR", "IGIB", "IGLB", "IGSB", "IJR", "ILTB", "IPAC", "ISTB", "ITOT", "IUSB", "IUSG", "IUSV", "IVV", "IVW", "IWB", "IWV", "IXUS", "JEPI", "JEPQ", "JHMM", "JPSE", "MGK", "MGV", "MUB", "NET", "PFF", "QQQ", "QQQE", "QQQM", "QYLD", "REET", "RSP", "RWR", "SCHB", "SCHD", "SCHG", "SCHH", "SCHK", "SCHX", "SCHY", "SGOL", "SGOV", "SIVR", "SLQD", "SPHD", "SPHY", "SPLG", "SPLV", "SPTL", "SPTM", "SPY", "SPYD", "STIP", "TFLO", "TIP", "TLT", "USHY", "USRT", "VAW", "VB", "VBK", "VBR", "VCIT", "VCLT", "VCR", "VCSH", "VDC", "VDE", "VEA", "VEU", "VFH", "VGSH", "VGT", "VHT", "VIG", "VIS", "VMBS", "VNQ", "VNQI", "VO", "VOE", "VONE", "VOO", "VOT", "VOX", "VPU", "VSS", "VT", "VTEB", "VTI", "VTIP", "VTV", "VUG", "VUSB", "VV", "VWO", "VXF", "VXUS", "VYM", "VYMI", "XLC", "XLE"]
 
 for symbol in symbols:
     time.sleep(5)
@@ -21,9 +23,9 @@ for symbol in symbols:
     ticker = yf.Ticker(symbol)
 
     # get stock info
-    with open("../data/symbols/" + symbol + '.json', 'w') as outfile:
+    with open("../data/yf/" + symbol + '.json', 'w') as outfile:
         json.dump(ticker.info, outfile, indent=4, sort_keys=True)
 
     # get historical market data
     history = ticker.history(period="max")
-    history.to_csv("../data/symbols/" + symbol + ".csv")
+    history.to_csv("../data/yf/" + symbol + ".csv")
