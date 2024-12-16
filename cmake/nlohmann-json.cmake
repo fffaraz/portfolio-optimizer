b2df4d350c26581983196887fd20852dfae5f4b0
Faraz Fallahi <fffaraz@gmail.com>
2024-12-15 16:28:44 -0800

    update

---
+++ cmake/nlohmann-json.cmake
@@ -0,0 +1,6 @@
+include(FetchContent)
+
+set(JSON_BuildTests OFF CACHE INTERNAL "")
+
+FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.11.3/json.tar.xz)
+FetchContent_MakeAvailable(json)
