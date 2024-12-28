d2c863f65e75adf82dd470dc0f24728c3e9174a9
Faraz Fallahi <fffaraz@gmail.com>
2024-12-28 03:27:39 -0800

    Update OhlcListTest.cpp

--- test/OhlcListTest.cpp
+++ test/OhlcListTest.cpp
@@ -13,9 +13,9 @@ using namespace Farazlib;
 
 TEST(OhlcList, sameDay)
 {
-    Ohlc ohlc1 { 95, 100, 90, 92 };
-    Ohlc ohlc0 { 95, 100, 90, 98 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 95, 100, 90, 92 };
+    const Ohlc ohlc0 { 95, 100, 90, 98 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Up, list.priceDirection(0, 0));
     EXPECT_EQ(3.0 / 95, list.priceChange(0, 0, PriceType::Close));
     EXPECT_EQ(PriceDirection::Down, list.priceDirection(1, 0));
@@ -34,9 +34,9 @@ TEST(OhlcList, priceDirection1)
             |
             -
     */
-    Ohlc ohlc1 { 95, 100, 90, 95 };
-    Ohlc ohlc0 { 75, 80, 70, 75 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 95, 100, 90, 95 };
+    const Ohlc ohlc0 { 75, 80, 70, 75 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::VeryDown, list.priceDirection(0, 1));
 }
 
@@ -51,9 +51,9 @@ TEST(OhlcList, priceDirection2)
             |
             -
     */
-    Ohlc ohlc1 { 95, 100, 90, 95 };
-    Ohlc ohlc0 { 85, 90, 80, 85 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 95, 100, 90, 95 };
+    const Ohlc ohlc0 { 85, 90, 80, 85 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::VeryDown, list.priceDirection(0, 1));
 }
 
@@ -67,9 +67,9 @@ TEST(OhlcList, priceDirection3)
             |
             -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 85, 95, 80, 85 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 85, 95, 80, 85 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Down, list.priceDirection(0, 1));
 }
 
@@ -81,9 +81,9 @@ TEST(OhlcList, priceDirection4)
         |   |
         -   -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 100, 110, 90, 105 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 100, 110, 90, 105 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Narrow, list.priceDirection(0, 1));
 }
 
@@ -97,9 +97,9 @@ TEST(OhlcList, priceDirection5)
         -   |
             -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 100, 120, 80, 105 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 100, 120, 80, 105 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Widen, list.priceDirection(0, 1));
 }
 
@@ -112,9 +112,9 @@ TEST(OhlcList, priceDirection6)
         |   |
         -   -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 100, 120, 90, 105 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 100, 120, 90, 105 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Up, list.priceDirection(0, 1));
 }
 
@@ -127,9 +127,9 @@ TEST(OhlcList, priceDirection7)
         |   -
         -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 100, 120, 95, 105 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 100, 120, 95, 105 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::Up, list.priceDirection(0, 1));
 }
 
@@ -143,8 +143,8 @@ TEST(OhlcList, priceDirection8)
         |
         -
     */
-    Ohlc ohlc1 { 100, 110, 90, 105 };
-    Ohlc ohlc0 { 115, 120, 110, 115 };
-    OhlcList list { { ohlc0, ohlc1 } };
+    const Ohlc ohlc1 { 100, 110, 90, 105 };
+    const Ohlc ohlc0 { 115, 120, 110, 115 };
+    const OhlcList list { { ohlc0, ohlc1 } };
     EXPECT_EQ(PriceDirection::VeryUp, list.priceDirection(0, 1));
 }
