38c2bd8e69fb65bad3145392867dca7b8fb73b8a
Faraz Fallahi <fffaraz@gmail.com>
2025-02-23 13:55:37 -0800

    1

---
+++ cmake/libassert.cmake
@@ -0,0 +1,7 @@
+include(FetchContent)
+FetchContent_Declare(
+  libassert
+  GIT_REPOSITORY https://github.com/jeremy-rifkin/libassert.git
+  GIT_TAG        v2.1.5
+)
+FetchContent_MakeAvailable(libassert)
