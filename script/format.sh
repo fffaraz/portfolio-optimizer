2a997446d11786ebd7c5fa9966f0eb2c58d56e08
Faraz Fallahi <fffaraz@gmail.com>
2025-03-02 13:58:14 -0800

    Update format.sh

--- script/format.sh
+++ script/format.sh
@@ -7,7 +7,7 @@ $CLANG_FORMAT --version
 
 find . -not -path "./build/*" -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec $CLANG_FORMAT --verbose -style=file -i {} \;
 
-# pip3 install --user cmake-format
+# pip3 install --upgrade --user cmake-format
 cmake-format --version
 
 find . -not -path "./build/*" -name 'CMakeLists.txt' -exec cmake-format -i {} \;
